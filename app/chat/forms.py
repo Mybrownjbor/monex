862d5e5c3dddec34ad67da5e5cc017482e2a997f
uuganbat <uuganbat@innosol.mn>
2016-05-10 14:33:38 +0900

    monex

--- app/chat/forms.py
+++ app/chat/forms.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 from django import forms
 from .models import *
 
@@ -9,6 +11,12 @@ class RoomForm(forms.ModelForm):
 	class Meta:
 		model = Room
 		fields = '__all__'
+		widgets = {
+			'name' : forms.TextInput(attrs = {'class':'form-control'})
+		}
+		labels = {
+			'name' : u'Форум'
+		}
 
 class MessageForm(forms.ModelForm):
 
@@ -17,5 +25,8 @@ class MessageForm(forms.ModelForm):
 		fields = ['content']
 		widgets = {
 			'content' : forms.Textarea(attrs = {'class': 'form-control'}),
+		}
+		labels = {
+			'content' : u'Мессеж'
 		}
 		
